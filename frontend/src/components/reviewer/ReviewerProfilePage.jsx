f918619db23dce453751b49f60510a6cc8450746
JAM3S11 <jdndirangu2020@gmail.com>
2026-02-04 22:18:13 +0300

    Pushed due to small cursor edits configurations

--- frontend/src/components/reviewer/ReviewerProfilePage.jsx
+++ frontend/src/components/reviewer/ReviewerProfilePage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DashboardLayout from '../ui/DashboardLayout'
 import { useAuthenticationStore } from '../../store/authStore'
 import { useProfileStore } from "../../store/profileStore";
@@ -6,6 +6,13 @@ import ProfileSettings from '../ui/ProfileSettings'
 import toast from 'react-hot-toast';
 
 const ReviewerProfilePage = () => {
+  const { user } = useAuthenticationStore();
+  const { personal, contact, getProfile, putProfile, loading, setUser } = useProfileStore();
+
+  const [personalData, setPersonalData] = useState({
+    name: "",
+    role: ""
+  });
   return (
     <DashboardLayout>
         <ProfileSettings />

20c4d989fee91a197fc952a0652e690822ab4a8f
JAM3S11 <jdndirangu2020@gmail.com>
2026-02-06 17:31:54 +0300

    Canvas logo implementation for SOLEASE

--- frontend/src/common/Header.jsx
+++ frontend/src/common/Header.jsx
@@ -1,6 +1,52 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Menu, X, ArrowRight, User } from "lucide-react";
 import { Link } from "react-router";
+
+const CanvasLogo = ({ isBlurred }) => {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+
+    if(!canvas) return;
+
+    const ctx = canvas.getContext('2d');
+
+    // CLear for redrawing
+    ctx.clearRect(0, 0, 40, 40);
+
+    // Background gradient based on the scroll state
+    const gradient = ctx.createLinearGradient(0, 0, 40, 40);
+    if(isBlurred){
+      gradient.addColorStop(0, '#2563EB');
+      gradient.addColorStop(1, '#06B6D4');
+    } else {
+      gradient.addColorStop(0, '#FFFFFF');
+      gradient.addColorStop(1, '#93C5FD');
+    }
+
+    ctx.fillStyle = gradient;
+
+    // Drew the S-shape layer
+    const drawChevron = (yOffset, alpha = 1) => {
+      ctx.globalAlpha = alpha;
+      ctx.beginPath();
+      ctx.moveTo(10, yOffset + 8);
+      ctx.lineTo(20, yOffset);
+      ctx.lineTo(30, yOffset + 8);
+      ctx.lineTo(20, yOffset + 16);
+      ctx.fill();
+    }
+
+    drawChevron(18, 0.6); // Bottom
+    drawChevron(10, 0.8); // Middle
+    drawChevron(2, 1);    // Top
+
+    ctx.globalAlpha = 1;
+  }, [isBlurred]);
+
+  return <canvas ref={canvasRef} width='40' height='40' className="w-8 h-8 md:w-10 md:h-10" />
+}
 
 const Header = () => {
   const [isBlurred, setIsBlurred] = useState(false);
@@ -39,7 +85,10 @@ const Header = () => {
       <nav className="max-w-7xl mx-auto flex items-center justify-between px-6 md:px-10">
         {/* Logo */}
         <div className="text-3xl md:text-4xl font-black tracking-tighter text-blue-600 drop-shadow-sm">
-          <Link to="/" onClick={onScrollToTop}>SOLEASE</Link>
+          <Link to="/" onClick={onScrollToTop} className="flex m-0">
+            <CanvasLogo isBlurred={isBlurred} />{" "}
+            <span className="hidden md:block">SOLEASE</span>
+          </Link>
         </div>
 
         {/* Desktop Navigation */}
